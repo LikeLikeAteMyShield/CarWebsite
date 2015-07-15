9408ea9047614c293e44e08f6b37ba9714a8d7a4
LikeLikeAteMyShield <jonnycameron17@gmail.com>
2015-07-15 13:20:09 +0100

    adding a second test car and creating function to loop through and display them

--- javascripts/cars.js
+++ javascripts/cars.js
@@ -1,3 +1,16 @@
 var cars = [
-	{"make" : "Volkswagen", "model" : "Golf", "year" : 2010, "type" : "Hatchback", "doors" : 3, "miles" : 70000, "colour" : "Black", "price" : 4995.00, "date-added" : "2015-07-15"}
+	{"make" : "Volkswagen", "model" : "Golf", "year" : 2010, "type" : "Hatchback", "doors" : 3, "miles" : 70000, "colour" : "Black", "price" : 4995.00, "date-added" : "2015-07-15"},
+	{"make" : "Ford", "model" : "Mondeo", "year" : 2010, "type" : "Hatchback", "doors" : 3, "miles" : 70000, "colour" : "Black", "price" : 4995.00, "date-added" : "2015-07-15"}
 ];
+
+function displayCars(arr) {
+
+	var out = "";
+	var i;
+	for(i=0 ; i<arr.length ; i++) {
+		out += "<p>" + arr[i].make + " " + arr[i].model + "</p>";
+	}
+	document.getElementById("cars").innerHTML = out;
+}
+
+displayCars(cars);
