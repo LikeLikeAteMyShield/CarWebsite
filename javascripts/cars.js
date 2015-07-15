26542c27626fc0c90a999d9db7a872c2350ae76b
LikeLikeAteMyShield <jonnycameron17@gmail.com>
2015-07-15 13:06:03 +0100

    changing data to cars

--- javascripts/cars.js
+++ javascripts/cars.js
@@ -1,3 +1,3 @@
-var data = "cars" : [
+var cars = [
 	{"make" : "Volkswagen", "model" : "Golf", "year" : 2010, "type" : "Hatchback", "doors" : 3, "miles" : 70000, "colour" : "Black", "price" : 4995.00, "date-added" : "2015-07-15"}
 ];
